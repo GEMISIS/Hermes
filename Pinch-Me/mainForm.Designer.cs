164e73cc7b698447fc7f8da823b8a77a94bb2ba1
Gerald McAlister <gerald.mcalister.10@cnu.edu>
2014-11-30 01:28:15 -0500

    Initial Demo
    -Initial Demo.  Simply renders color frame data to the screen.

---
+++ Pinch-Me/mainForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Pinch_Me
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.mainImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainImage
+            // 
+            this.mainImage.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.mainImage.Location = new System.Drawing.Point(12, 12);
+            this.mainImage.Name = "mainImage";
+            this.mainImage.Size = new System.Drawing.Size(600, 417);
+            this.mainImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mainImage.TabIndex = 0;
+            this.mainImage.TabStop = false;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.mainImage);
+            this.Name = "mainForm";
+            this.Text = "Pinch Me";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.mainForm_FormClosed);
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.mainImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainImage;
+    }
+}
+
---
+++ Pinch-Me/mainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libpxcclr.cs;
+
+namespace Pinch_Me
+{
+    public partial class mainForm : Form
+    {
+        PXCMSession session;
+        PXCMSenseManager manager;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            session = PXCMSession.CreateInstance();
+            manager = PXCMSenseManager.CreateInstance();
+
+            if (manager == null)
+            {
+                Console.WriteLine("Failed");
+            }
+            manager.EnableStream(PXCMCapture.StreamType.STREAM_TYPE_COLOR, 1920, 1080, 30);
+            manager.Init();
+
+            Timer t = new Timer();
+            t.Tick += t_Tick;
+            t.Start();
+        }
+
+        void t_Tick(object sender, EventArgs e)
+        {
+            if(manager.AcquireFrame(true) < pxcmStatus.PXCM_STATUS_NO_ERROR)
+            {
+                return;
+            }
+            PXCMCapture.Sample sample = manager.QuerySample();
+            this.newColorFrame(0, sample);
+            manager.ReleaseFrame();
+        }
+
+        pxcmStatus newColorFrame(int mid, PXCMCapture.Sample sample)
+        {
+            if(sample.color != null)
+            {
+                PXCMImage.ImageData data = new PXCMImage.ImageData();
+                sample.color.AcquireAccess(PXCMImage.Access.ACCESS_READ, PXCMImage.PixelFormat.PIXEL_FORMAT_RGB32, out data);
+                mainImage.Image = data.ToBitmap(0, sample.color.info.width, sample.color.info.height);
+            }
+            return pxcmStatus.PXCM_STATUS_NO_ERROR;
+        }
+
+        private void mainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            manager.Close();
+            manager.Dispose();
+            session.Dispose();
+        }
+    }
+}
