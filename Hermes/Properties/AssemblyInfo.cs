cc7980988138d10e5babba62564e2fa9448c38ea
Gerald McAlister <gerald.mcalister.10@cnu.edu>
2014-12-16 22:53:29 -0500

    Updated Project Name
    -Updated project name and stuff

--- Hermes/Properties/AssemblyInfo.cs
+++ Hermes/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Pinch-Me")]
+[assembly: AssemblyTitle("Hermes")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Pinch-Me")]
+[assembly: AssemblyProduct("Hermes")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("57d30e39-337a-4487-85ed-bc78a9710906")]
+[assembly: Guid("7fb3f0d2-3089-4de3-b801-8e8ff8896d67")]
 
 // Version information for an assembly consists of the following four values:
 //
